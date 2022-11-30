0e56761018cfe8453d0481d361b66e092710eb8e
martafralves <74558958+martafralves@users.noreply.github.com>
2022-11-30 21:24:38 +0000

    Search page styled

--- client/src/views/Search.js
+++ client/src/views/Search.js
@@ -1,8 +1,9 @@
 import SearchBar from "../components/SearchBar"
+import '../styles/search.css'
 
 function Search() {
   return (
-    <div>
+    <div className="search-container">
       <h3 className = 'search-header'>Search for a book</h3>
         <SearchBar/>
     </div>
